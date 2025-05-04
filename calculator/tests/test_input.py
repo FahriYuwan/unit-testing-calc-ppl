dc2e2a6757b631fb098ddf05075d2763025540a2
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-05-04 11:38:51 +0700

    fixed

--- calculator/tests/test_input.py
+++ calculator/tests/test_input.py
@@ -1,7 +1,11 @@
 from calculator.get_input import get_input
 
-def test_input():
+def test_input(monkeypatch):
+    inputs = iter(['1', '2', '+'])
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    
     operand1, operand2, operator = get_input()
+    
     assert operand1 == '1'
     assert operand2 == '2'
     assert operator == '+'
